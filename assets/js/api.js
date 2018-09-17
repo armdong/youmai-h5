d99683660c5b6a629a072a03beef78c5246b134c
armdong <armdong@163.com>
2018-09-17 22:41:53 +0800

    modified

--- assets/js/api.js
+++ assets/js/api.js
@@ -2,8 +2,10 @@
   
   function fetchData(callback){
     $.ajax({
-      url: "http://120.25.226.242:8084/umer-extend/api/extend/getAct2018920Stat",
-      type: "POST",
+      // url: "http://120.25.226.242:8084/umer-extend/api/extend/getAct2018920Stat",
+      // type: "POST",
+      url: "assets/data/api.json",
+      type: "GET",
       dataType: "json",
       data: { umerId: "10816181" },
       success: function(data) {
